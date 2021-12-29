56ba970d543e90baf2dd065775661634daf9d82d
Siyamul Islam <53119070+siyamulislam@users.noreply.github.com>
2021-12-29 18:16:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Country from './components/Country/Country';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  //        
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/siyamulislam/countries/main/v3.1/countries.json')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(error => console.log(error))
+  }, []);
+
+  // const handleAddCountry = (country) => {
+  //   const newCart = [...cart, country];
+  //   setCart(newCart);
+  //  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>country loaded: {countries.length}</h1>
+
+      {/* <h4>Country added: {cart.length}</h4> */}
+      {/* <Cart cart={cart}></Cart> */}
+      { 
+        countries.map(country => {
+         // console.log(country.cca3);
+         return <Country key={country.cca3} country={country}></Country>
+        })
+      }
+
     </div>
   );
 }
